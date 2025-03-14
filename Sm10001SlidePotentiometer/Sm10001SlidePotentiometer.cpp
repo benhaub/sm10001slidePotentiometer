055567b98cd7a599d3b4837634cd8182f5ec41e7
Ben Haubrich <bjh885@mail.usask.ca>
2025-03-14 18:55:55 -0400

    Use the initSlidePot function and check for platforms (like raspberry pi) which don't implement the ADC.

--- Sm10001SlidePotentiometer/Sm10001SlidePotentiometer.cpp
+++ Sm10001SlidePotentiometer/Sm10001SlidePotentiometer.cpp
@@ -5,24 +5,13 @@
 #include "AdcModule.hpp"
 
 ErrorType Sm10001SlidePotentiometer::slidePotentiometerThread() {
-    std::unique_ptr<Sm10001Drivers::HBridge> hBridge = std::make_unique<Sm10001Drivers::Drv8872>();
-    assert(nullptr != hBridge.get());
-    std::unique_ptr<Adc> adc = std::make_unique<Adc>();
-    assert(nullptr != adc.get());
-    adc->peripheralNumber() = PeripheralNumber::Zero;
-    adc->channel() = AdcTypes::Channel::Four;
-    assert(ErrorType::Success == adc->init());
-    std::vector<GptmPwmModule> gptPwms;
-    gptPwms.reserve(2);
-    gptPwms.emplace_back(GptmPwmModule());
-    gptPwms.at(0).peripheralNumber() = PeripheralNumber::Zero;
-    gptPwms.emplace_back(GptmPwmModule());
-    gptPwms.at(1).peripheralNumber() = PeripheralNumber::One;
+    ErrorType error = ErrorType::Failure;
+    std::unique_ptr<Sm10001> sm10001;
 
-    hBridge->setPwms(gptPwms);
-
-    Sm10001 sm10001(hBridge, adc, PinNumber(SLIDE_POT_PIN_A), PinNumber(SLIDE_POT_PIN_B));
-    assert(ErrorType::Success == sm10001.init());
+    if (ErrorType::Success != (error = initSlidePot(PWM_TYPE, sm10001))) {
+        PLT_LOGE(TAG, "Failed to initialize slide potentiometer <error: %u>", (uint8_t)error);
+        return error;
+    }
 
     Volts potentiometerVoltageDrop = 0.0f;
     Volts previousVoltageDrop = 0.0f;
@@ -32,23 +21,37 @@ ErrorType Sm10001SlidePotentiometer::slidePotentiometerThread() {
         OperatingSystem::Instance().delay(Milliseconds(5000));
         assert(ErrorType::Success == sm10001.slideBackward());
         OperatingSystem::Instance().delay(Milliseconds(5000));
-        assert(ErrorType::Success == sm10001.getVoltageDrop(potentiometerVoltageDrop));
-        if (std::abs(potentiometerVoltageDrop - previousVoltageDrop) > hysteresis) {
-            previousVoltageDrop = potentiometerVoltageDrop;
-            PLT_LOGI(TAG, "Voltage drop: %f", potentiometerVoltageDrop);
+        if (ErrorType::Success == (error = sm10001->getVoltageDrop(potentiometerVoltageDrop))) {
+            if (std::abs(potentiometerVoltageDrop - previousVoltageDrop) > hysteresis) {
+                previousVoltageDrop = potentiometerVoltageDrop;
+                PLT_LOGI(TAG, "Voltage drop: %f", potentiometerVoltageDrop);
+            }
+        }
+        else {
+            const bool isCriticalError = !(error == ErrorType::NotImplemented || error == ErrorType::NotSupported || error == ErrorType::NotAvailable);
+            if (isCriticalError) {
+                PLT_LOGE(TAG, "Failed to get voltage drop: %d", (uint8_t)error);
+                return error;
+            }
         }
     }
 }
 
 ErrorType Sm10001SlidePotentiometer::initSlidePot(Sm10001SlidePotentiometerTypes::PwmType pwmType, std::unique_ptr<Sm10001> &sm10001) {
     if (pwmType == Sm10001SlidePotentiometerTypes::PwmType::Gptm) {
+        ErrorType error = ErrorType::Failure;
+
         std::unique_ptr<Sm10001Drivers::HBridge> hBridge = std::make_unique<Sm10001Drivers::Drv8872>();
         assert(nullptr != hBridge.get());
         std::unique_ptr<Adc> adc = std::make_unique<Adc>();
         assert(nullptr != adc.get());
         adc->peripheralNumber() = PeripheralNumber::Zero;
         adc->channel() = AdcTypes::Channel::Four;
-        assert(ErrorType::Success == adc->init());
+        if (ErrorType::Success != (error = adc->init())) {
+            if (ErrorType::NotImplemented == error) {
+                PLT_LOGI(TAG, "Adc not implemented for this platform.");
+            }
+        }
         std::vector<GptmPwmModule> gptPwms;
         gptPwms.reserve(2);
         gptPwms.emplace_back(GptmPwmModule());
@@ -63,13 +66,19 @@ ErrorType Sm10001SlidePotentiometer::initSlidePot(Sm10001SlidePotentiometerTypes
         return ErrorType::Success;
     }
     else if (pwmType == Sm10001SlidePotentiometerTypes::PwmType::Standalone) {
+        ErrorType error = ErrorType::Failure;
+
         std::unique_ptr<Sm10001Drivers::HBridge> hBridge = std::make_unique<Sm10001Drivers::Drv8872>();
         assert(nullptr != hBridge.get());
         std::unique_ptr<Adc> adc = std::make_unique<Adc>();
         assert(nullptr != adc.get());
         adc->peripheralNumber() = PeripheralNumber::Zero;
         adc->channel() = AdcTypes::Channel::Four;
-        assert(ErrorType::Success == adc->init());
+        if (ErrorType::Success != (error = adc->init())) {
+            if (ErrorType::NotImplemented == error) {
+                PLT_LOGI(TAG, "Adc not implemented for this platform.");
+            }
+        }
         std::vector<Pwm> pwms;
         pwms.reserve(2);
         pwms.emplace_back(Pwm());
